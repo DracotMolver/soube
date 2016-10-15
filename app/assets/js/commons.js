77f45ffc748e70a2c1f72da1b53c992cab218aeb
dracotmolver <diego.albertom@yahoo.cl>
2016-10-15 15:54:54 -0300

    added ;

--- app/assets/js/commons.js
+++ app/assets/js/commons.js
@@ -2,29 +2,29 @@
  * @author Diego Alberto Molina Vera
  */
 // Template para manipural el DOM
-require('./template')
+require('./template');
 
 // Módulos
 const {
   ipcRenderer,
   remote,
   shell
-} = require('electron')
+} = require('electron');
 
-const metaData = require('musicmetadata')
-const execFile = require('child_process').execFile
-const fs = require('fs')
-const path = require('path')
+const metaData = require('musicmetadata');
+const execFile = require('child_process').execFile;
+const fs = require('fs');
+const path = require('path');
 
 // Globales
-global.CONFIG_FILE = `${remote.app.getPath('userData')}/config.json`
-global.SONG_FILE = `${remote.app.getPath('userData')}/listSong.json`
-global.LANG_FILE = path.join(__dirname, '..', 'files', 'lang.json')
+global.CONFIG_FILE = `${remote.app.getPath('userData')}/config.json`;
+global.SONG_FILE = `${remote.app.getPath('userData')}/listSong.json`;
+global.LANG_FILE = path.join(__dirname, '..', 'files', 'lang.json');
 
-global.jread = data => JSON.parse(fs.readFileSync(data, { encoding: 'utf8', flag: 'r' }))
+global.jread = data => JSON.parse(fs.readFileSync(data, { encoding: 'utf8', flag: 'r' }));
 global.jsave = (data, c) => {
-  fs.writeFileSync(data, JSON.stringify(c, null), { encoding: 'utf8', mode: 511, flag: 'w' })
-  return jread(data)
+  fs.writeFileSync(data, JSON.stringify(c, null), { encoding: 'utf8', mode: 511, flag: 'w' });
+  return jread(data);
 }
 
 module.exports = {
@@ -35,4 +35,4 @@ module.exports = {
   shell,
   path,
   fs
-}
+};
