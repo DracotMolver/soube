6a0bce03c1444c11f3bcdb0447c2bc10866a9999
dracotmolver <diego.albertom@yahoo.cl>
2016-10-16 21:16:53 -0300

    changed a css class

--- app/assets/js/listSongs.js
+++ app/assets/js/listSongs.js
@@ -60,7 +60,7 @@ function getDataSongAtPosition() {
 function createDefaultListView() {
   // Contenedor que se reptite para el títutlo, artista y album
   const child = $.clone('div', false).addClass('grid-33 mobile-grid-33 song-info').css('overflow:hidden;');
-  const parentContainer = $.clone('div', false).addClass('grid-100');
+  const parentContainer = $.clone('div', false).addClass('list-song-container');
   const f = document.createDocumentFragment();
   let title = null;
   let album = null;
@@ -73,6 +73,7 @@ function createDefaultListView() {
 
     f.appendChild(
       $.clone(parentContainer, true)
+      .attr({id: i})
       .data({
         position: i,
         artist: v.artist,
@@ -88,9 +89,45 @@ function createDefaultListView() {
   $('#list-songs').insert(f);
 }
 
-/**
- * Función generadora que retorna el la ruta del archivo a leer
- */
+// function createSecondView() {
+//   const songFiles = jread(SONG_FILE);
+//   const parentContainer = $.clone('div', false).addClass('second-view-container');
+//   const child = $.clone('div', false).addClass('grid-100');
+//   const child2 = $.clone('div', false).addClass('grid-33');
+//   const f = document.createDocumentFragment();
+//   let artist = null;
+//   let album = null;
+
+//   // Obtener el total de artistas unicos
+//   [...new Set(songFiles.map(v => v.artist.toLowerCase()))].forEach(v => {
+//     artist = $.clone(child, true).text(v).addClass('second-view-artist');
+
+//     // Obtener total de albunes únicos
+//     album = $.clone(child, true);
+//     [...new Set(songFiles.map(s.album.toLowerCase()))].filter(v => {
+//       if (v === ) {
+//       }
+//     });
+//         .insert(
+//           $.clone(child2, true).text(v).addClass('second-view-album')
+//         );
+//     // songFiles.filter(s => {
+//     //   if (s.artist.toLowerCase() === v) return s;
+//     // }).forEach(a => {
+//     //   console.log(a)
+//     // });
+
+//     f.appendChild(
+//       $.clone(parentContainer, true)
+//       // .attr({id: i})[0]
+//       .insert(artist, album)[0]
+//     );
+//   });
+
+//   $('#list-songs').insert(f);
+// }
+
+// Función generadora que retorna el la ruta del archivo a leer
 const iter = (function* getSongs() { 
   while (songSize--) yield files[songSize]
 })();
@@ -134,12 +171,8 @@ function getMetadata(folder, _fnStart, _fnEnd, _fnIter) {
   });
 }
 
-/**
- * Extrae los datos de las canciones.
- * Usamos MUSICMETADATA de @leetreveil - npm
- *
- * @var iter {Iterador} - Iterador devuelto por la funcion generador
- */
+// Extrae los datos de las canciones.
+// Usamos MUSICMETADATA de @leetreveil - npm
 function extractMetadata() {
   fnIter(++count, max);
   iterator = iter.next();
@@ -150,16 +183,16 @@ function extractMetadata() {
       metaDataSongs.push(
         error ?
           {
-            artist: lang.artist.replace(/\s/g, '&nbsp;'),
-            album: lang.album.replace(/\s/g, '&nbsp;'),
-            title: lang.title.replace(/\s/g, '&nbsp;'),
+            artist: lang.artist.trim().replace(/\s/g, '&nbsp;'),
+            album: lang.album.trim().replace(/\s/g, '&nbsp;'),
+            title: lang.title.trim().replace(/\s/g, '&nbsp;'),
             filename: iterator.value,
             position: metaDataSongs.length
           } :
           {
-            artist: (data.artist.length !== 0 ? data.artist[0] : lang.artist).replace(/\s/g, '&nbsp;'),
-            album: (data.album.trim().length !== 0 ? data.album : lang.album).replace(/\s/g, '&nbsp;'),
-            title: (data.title.trim().length !== 0 ? data.title : lang.title).replace(/\s/g, '&nbsp;'),
+            artist: (data.artist.length !== 0 ? data.artist[0] : lang.artist).trim().replace(/\s/g, '&nbsp;'),
+            album: (data.album.trim().length !== 0 ? data.album : lang.album).trim().replace(/\s/g, '&nbsp;'),
+            title: (data.title.trim().length !== 0 ? data.title : lang.title).trim().replace(/\s/g, '&nbsp;'),
             filename: iterator.value,
             position: metaDataSongs.length
           }
@@ -178,5 +211,6 @@ function avoidStack () {
 module.exports = {
   setNextSongFunction,
   createDefaultListView,
+  // createSecondView,
   getMetadata
 };
