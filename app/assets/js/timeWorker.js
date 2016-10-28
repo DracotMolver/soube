9748c294a8b6ff8af804ae93d73d2d0b42e3825f
DracotMolver <dracotm25@gmail.com>
2016-10-28 00:35:48 -0300

    Implementing workers

---
+++ app/assets/js/timeWorker.js
@@ -0,0 +1,39 @@
+/**
+ * @author Diego Molina Vera.
+ * 
+ * Calcula el tiempo que transcurre mientras se reproduce la canción
+ */
+let millisecond = 0;
+let second = 0;
+let minute = 0;
+let percent = 0;
+
+function start(lapse) {
+  ++millisecond;
+  if (millisecond > 59) { // Segundos
+    ++second;
+    millisecond = 0;
+    if (second > 59) {
+      ++minute;
+      second = 0;
+    }
+
+    // Tiempo transcurrido
+    postMessage({
+      time: `${minute > 9 ? `${minute}` : `0${minute}`}${second > 9 ? `:${second}` : `:0${second}`}`,
+      w: `width:${percent += lapse}%`
+    });
+  }
+}
+
+function stop() {
+  millisecond = second = minute = percent = 0;
+}
+
+this.onmessage = e => {
+  switch(e.data.action) {
+    case 'start':
+    case 'resume': start(e.data.per); break;
+    case 'stop': stop(); break;
+  }
+};
