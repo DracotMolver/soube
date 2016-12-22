d2addd8372ee10c4853a160e3f93326ddef39578
DracotMolver <dracotm25@gmail.com>
2016-12-22 20:34:51 -0300

    It was changed the whole DOM framework for an small one

--- app/assets/js/dom/index.js
+++ app/assets/js/dom/index.js
@@ -2,265 +2,173 @@
  * Framework pequeño para manipular el DOM
  * @author Diego Alberto Molina Vera
  */
-
 module.exports = (_ => {
   /* --------------------------------- Variables --------------------------------- */
-  let poolElements = [];
-  let listeners = {
-    key: ''
-  };
-  let poolListeners = [];
-  let count = 0;
+  var createdElements = {};
+  var poolOfElements = {};
 
-  /* --------------------------------- Pools --------------------------------- */
-  // Listeners
-  const registerListener = (name, listener) => {
-    listener.key = /^[^\.\:\#]/.test(name) ? `#${name}` : name;
-    poolListeners[listener.key] = listener;
-  }
+  /* --------------------------------- Eventos --------------------------------- */
+  const Event = {
+    element: null,
+    addClass: str => {
+      const rgx = new RegExp(str, 'g');
+      const cName = Event.element.className;
 
-  const getListener = name => {
-    if (/^[^\.\:\#]/.test(name)) name = `#${name}`;
-    return poolListeners[name];
-  };
+      if (!rgx.test(cName)) Event.element.className += cName === '' ? `${str}` : ` ${str}`;
 
-  // Pool general
-  const setPool = (name, element) => {
-    if (/^[^\.\:\#]/.test(name)) name = `#${name}`;
-
-    if (!poolElements[name]) poolElements[name] = element;
-  };
-
-  const getPool = name => {
-    if (/^[^\.\:\#]/.test(name)) name = `#${name}`;
-    return poolElements[name] ? poolElements[name] : false;
-  };
-
-  const isInPool = name => {
-    if (/^[^\.\:\#]/.test(name)) name = `#${name}`;
-    return Object.keys(poolElements).some(v => v === name);
-  };
-
-  /* --------------------------------- Events --------------------------------- */
-  const emitters = {
-    events: {},
-    on: (name, fn) => {
-      emitters.events[name] = fn;
+      return Event;
     },
-    send: (name, ...args) => {
-      return emitters.events[name].apply(null, args);
-    }
-  };
+    text: (str = '') => {
+      if (str !== '') Event.element.innerHTML = `${str}`;
+      else return Event.element.textContent;
 
-  emitters.on('addClass', (name, str) => {
-    let el = getPool(name);
-    const rgx = new RegExp(str, 'g');
-    const cName = el.className;
-    if (!rgx.test(cName)) el.className += cName === '' ? `${str}` : ` ${str}`;
+      return Event;
+    },
+    replaceClass: (from, to) => {
+      Event.element.className = Event.element.className.replace(from, to);
+      return Event;
+    },
+    child: (pos = -1) => {
+      if (pos !== -1) Event.element = Event.element.children[pos];
+      else Event.element = Array.from(Event.element.children[0]);
 
-    return getListener(name);
-  });
-
-  emitters.on('text', (name, str = null) => {
-    let el = getPool(name);
-
-    if (str !== null) el.innerHTML = `${str}`;
-    else return el.textContent;
-
-    return getListener(name);
-  });
-
-  emitters.on('replaceClass', (name, from, to) => {
-    let el = getPool(name);
-    el.className = el.className.replace(from, to);
-
-    return getListener(name);
-  });
-
-  emitters.on('child', (name, pos = -1) => {
-    if (pos > -1) {
-      let c = getPool(name).children[pos];
-      name = `${name}-child`;
-      setPool(name, c);
-      registerListener(name, Object.assign({}, listeners));
-    } else {
-      return Array.from(getPool(name).children);
-    }
-
-    return getListener(name);
-  });
-
-  emitters.on('on', (name, fn) => {
-    if (getPool(name).length !== undefined) {
-      getPool(name).forEach(e => {
+      return Event;
+    },
+    on: fn => {
+      if (Event.element.length !== undefined) {
+        Event.element.forEach(e => {
+          Object.keys(fn).forEach(v => {
+            /animation/.test(v) ?
+              e.addEventListener(v.toLowerCase(), fn[v]) :
+              e[`on${v.toLowerCase()}`] = fn[v];
+          });
+        });
+      } else {
         Object.keys(fn).forEach(v => {
           /animation/.test(v) ?
-            e.addEventListener(v.toLowerCase(), fn[v]) :
-            e[`on${v.toLowerCase()}`] = fn[v];
+            Event.element.addEventListener(v.toLowerCase(), fn[v]) :
+            Event.element[`on${v.toLowerCase()}`] = fn[v];
         });
-      });
-    } else {
-      Object.keys(fn).forEach(v => {
-        /animation/.test(v) ?
-          getPool(name).addEventListener(v.toLowerCase(), fn[v]) :
-          getPool(name)[`on${v.toLowerCase()}`] = fn[v];
-      });
-    }
-  });
+      }
 
-  emitters.on('data', (name, data) => {
-    if (typeof data === 'string') {
-      let d = getPool(name).dataset[data];
-      if (/^\d+$/.test(d)) return parseInt(d);
-      else if (/^\d+(\.+)\d+$/.test(d)) return parseFloat(d);
-      else if (/^(\w|\s)+$/.test(d)) return d.toString();
-    } else {
-      let el = getPool(name);
-      Object.keys(data[0]).forEach(v => {
-        el.dataset[v] = data[v];
-      });
-    }
+      return Event;
+    },
+    data: (data = null) => {
+      if (data !== null) {
+        let d = Event.element.dataset[data];
+        if (/^\d+$/.test(d)) return parseInt(d);
+        else if (/^\d+(\.+)\d+$/.test(d)) return parseFloat(d);
+        else if (/^(\w|\s)+$/.test(d)) return d.toString();
+      } else {
+        Object.keys(data[0]).forEach(v => {
+          Event.element.dataset[v] = data[v];
+        });
+      }
 
-    return getListener(name);
-  });
-
-  emitters.on('each', (name, fn) => {
-    let el = getPool(name);
-    el.forEach((v, i) => {
+      return Event;
+    },
+    each: fn => {
+      Event.element.forEach((v, i) => {
       fn.length === 1 ? fn(v) : fn(v, i);
-    });
-
-    return getListener(name);
-  });
-
-  emitters.on('css', (name, str) => {
-    const rgx = new RegExp(str, 'g');
-    let cssText = null;
-    let el = getPool(name);
-
-    if (el.length !== undefined) {
-      el.forEach(e => {
-        cssText = e.style;
-        if (!rgx.test()) cssText.cssText += cssText.cssText === '' ? `${str};` : ` ${str};`;
       });
-    } else {
-      cssText = el.style;
-      if (!rgx.test()) cssText.cssText += cssText.cssText === '' ? `${str};` : ` ${str};`;
+
+      return Event;
+    },
+    css: str => {
+      const rgx = new RegExp(str, 'g');
+      let el = Event.element;
+
+      if (el.length !== undefined) {
+        el.forEach(e => {
+          if (!rgx.test(e.style.cssText)) e.style.cssText += e.style.cssText === '' ? `${str};` : ` ${str};`;
+        });
+      } else {
+        if (!rgx.test(el.style.cssText)) el.style.cssText += el.style.cssText === '' ? `${str};` : ` ${str};`;
+      }
+
+      return Event;
+    },
+    clone: (name, isCloned) => {
+      Event.element = getCreatedElement(name).cloneNode(isCloned);
+      return Event;
+    },
+    get: () => Event.element,
+    rmAttr: attr => {
+      Event.element.removeAttribute(attr);
+      return Event;
     }
+  };
 
-    return getListener(name);
-  });
+  /* --------------------------------- Funciones --------------------------------- */
+  function saveCreatedElement(name) {
+    if (!createdElements[name]) createdElements[name] = document.createElement(name);
+  }
 
-  emitters.on('clone', (name, isCloned) => {
-    return DOM(getPool(name).cloneNode(isCloned));
-  });
+  function getCreatedElement(name) {
+    return createdElements[name];
+  }
 
-  emitters.on('get', (name, pos) => {
-    const e = getPool(name);
-    return e.length !== undefined ? e[pos] : e;
-  });
+  function saveElementInPool(name, element) {
+    poolOfElements[name] = element;
+  }
 
-  emitters.on('rmAttr', (name, attr) => {
-    getPool(name).removeAttribute(attr);
-    return getListener(name);
-  });
+  function inPool(name) {
+    return poolOfElements[name] === Event.element;
+  }
 
-  listeners.addClass = function (str) { return emitters.send('addClass', this.key, str); };
-  listeners.text = function (str) { return emitters.send('text', this.key, str); };
-  listeners.replaceClass = function (from, to) { return emitters.send('replaceClass', this.key, from, to); };
-  listeners.child = function (pos) { return emitters.send('child', this.key, pos); };
-  listeners.on = function (fn) { return emitters.send('on', this.key, fn); };
-  listeners.data = function (data) { return emitters.send('data', this.key, data); };
-  listeners.each = function (fn) { return emitters.send('each', this.key, fn); };
-  listeners.css = function (str) { return emitters.send('css', this.key, str); };
-  listeners.clone = function (isCloned) { return emitters.send('clone', this.key, str) };
-  listeners.get = function (pos) { return emitters.send('get', this.key, pos); }
-  listeners.rmAttr = function (attr) { return emitters.send('rmAttr', this.key, attr); }
+  function getElementInPool(name) {
+    return poolOfElements[name];
+  }
 
-  //   child: function (p = 'all') { // Buscar hijos
-  //     this._e = (p !== 'all' && typeof p === 'number') ? this._e.children[p] : Array.from(this._e.children);
-  //     return this;
-  //   },
-  //   rmChild: function (c) { // Remover hijo específico
-  //     this._e.removeChild(this._e.children[c - 1]);
-  //   },
+//   //   rmChild: function (c) { // Remover hijo específico
+//   //     this._e.removeChild(this._e.children[c - 1]);
+//   //   },
 
-  //   has: function (s) {
-  //     return (new RegExp(s, 'g')).test(this._e.className);
-  //   },
-  //   attr: function (o) { // Agrega un atributo
-  //     if (typeof o === 'object')
-  //       Object.keys(o).forEach(v => { this._e.setAttribute(v, o[v]); });
-  //     else if (typeof o == 'string')
-  //       return this._e.getAttribute(o);
-
-  //     return this;
-  //   },
-  //   insert: function (...a) { // appendChild
-  //     a.forEach(v => { this._e.appendChild('_e' in v ? v._e : v); });
-  //     return this;
-  //   },
-
-
-  //     return this;
-  //   },
-
-
-  //     return this;
-  //   },
-  //   val: function (v = '') { // Ingresar value
-  //     if (v === '') {
-  //       return v.value;
-  //     } else {
-  //       this._e.value = v;
-  //       return this;
-  //     }
-  //   },
-  // }
+//   //   has: function (s) {
+//   //     return (new RegExp(s, 'g')).test(this._e.className);
+//   //   },
+//   //   attr: function (o) { // Agrega un atributo
+//   //     if (typeof o === 'object')
+//   //       Object.keys(o).forEach(v => { this._e.setAttribute(v, o[v]); });
+//   //     else if (typeof o == 'string')
+//   //       return this._e.getAttribute(o);
+//   //     return this;
+//   //   },
+//   //   insert: function (...a) { // appendChild
+//   //     a.forEach(v => { this._e.appendChild('_e' in v ? v._e : v); });
+//   //     return this;
+//   //   },
+//   //   val: function (v = '') { // Ingresar value
+//   //     if (v === '') {
+//   //       return v.value;
+//   //     } else {
+//   //       this._e.value = v;
+//   //       return this;
+//   //     }
+//   //   },
+//   // }
 
   /* --------------------------------- Main Function --------------------------------- */
   // Recibe un string para buscar el elemento en el DOM y retornar un objeto
-  // con todasl a funciones necesarias para ser usados sobre este proyecto
+  // con todas las funciones necesarias para ser usados sobre este proyecto
   const DOM = e => {
     // Revisar si ya se está usando el mismo elemento
     // De no existir se crea, pero solo una vez.
-    if (!isInPool(e)) {
+    if (inPool(e)) {
+      Event.element = getElementInPool(e);
+      return Event;
+    } else {
       // Insertar en pool elementos seleccionados desde el DOM
-      if (/^\./.test(e)) setPool(e, Array.from(document.getElementsByClassName(e.replace('.', '')))), count = 0;
-      else if (/^#/.test(e)) setPool(e, document.getElementById(e.replace('#', ''))), count = 0;
-      else if (/^:/.test(e)) setPool(e, Array.from(document.getElementsByTagName(e.replace(':', '')))), count = 0;
-      else {
-        if (typeof e !== 'object') { // Cuando pasamos un string para crear un elemento
-          setPool(e, document.createElement(e));
-        } else { // Cando se pasa el objeto de manera directa usando this
-          if (e.id === '') {
-            e.id = `${e.tagName.toLowerCase()}-${count++}`;
-            const search = () => {
-              if (isInPool(e.id)) {
-                e.id = `${e.tagName.toLowerCase()}-${parseInt(e.id.match(/\d+$/)[0]) + 1}`;
-                search();
-              } else {
-                return;
-              }
-            };
-            search();
-          }
+      if (/^\./.test(e)) e = Array.from(document.getElementsByClassName(e.replace('.', '')));
+      else if (/^#/.test(e)) e = document.getElementById(e.replace('#', ''));
+      else if (/^:/.test(e)) e = Array.from(document.getElementsByTagName(e.replace(':', '')));
+      else if (typeof e !== 'object') saveCreatedElement(e);
 
-          if (e !== document) {
-            setPool(`${e.id}`, e);
-            e = `${e.id}`;
-          } else {
-            setPool('document', e);
-            e = 'document';
-          }
-        }
-      }
-
-      registerListener(e, Object.assign({}, listeners));
+      Event.element = e;
     }
 
-    return getListener(e);
-  };
+    return Event;
+  }
 
   _.$ = DOM; // Manipular DOM
 })(global);
