a7cdf6120b8a70e50e5335f7ff471c8b5a3980a1
DracotMolver <diego.albertom@yahoo.cl>
2017-04-01 17:22:59 -0300

    Removed a const regex due weird behaviour

--- app/assets/js/player/addSongFolder/index.js
+++ app/assets/js/player/addSongFolder/index.js
@@ -37,25 +37,25 @@ function findFiles(dir) {
   let foldersSize = 0;
   let folders = [];
   let baseFolder = '';
-  const RGX_EXT = /(\.mp3|\.wmv|\.wav|\.ogg)$/ig;
 
   fs.readdirSync(dir).forEach(files => {
     // Based folders
     baseFolder = path.join(dir, files);
     if (fs.lstatSync(baseFolder).isDirectory()) {
       folders.push(baseFolder);
-    } else if (fs.lstatSync(baseFolder).isFile() && RGX_EXT.test(files)) {
+    } else if (fs.lstatSync(baseFolder).isFile() && /\.(mp3|wmv|wav|ogg)$/ig.test(baseFolder.trim())) {
       allFiles.push(baseFolder);
     }
   });
 
   foldersSize = folders.length - 1;
+  var count = 0;
   while (foldersSize > -1) {
     fs.readdirSync(folders[foldersSize]).forEach(files => {
       baseFolder = path.join(folders[foldersSize], files);
       if (fs.lstatSync(baseFolder).isDirectory()) {
         tmpFolders.push(baseFolder);
-      } else if (fs.lstatSync(baseFolder).isFile() && RGX_EXT.test(files)) {
+      } else if (fs.lstatSync(baseFolder).isFile() && /\.(mp3|wmv|wav|ogg)$/ig.test(baseFolder.trim())) {
         allFiles.push(baseFolder);
       }
     });
