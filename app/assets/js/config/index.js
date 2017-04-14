124948daf6946b66062feaf78d3f3dcf4c5a4d81
DracotMolver <diego.albertom@yahoo.cl>
2017-04-14 00:45:33 -0300

    The EQ values need to be change it

--- app/assets/js/config/index.js
+++ app/assets/js/config/index.js
@@ -53,8 +53,16 @@ function createFiles(app) {
     version(net, actualVersion, response => {
       if (response === 'major') {
         let config = JSON.parse(fs.readFileSync(configPath).toString());
-        config.musicFolder = [config.musicFolder];
-        fs.writeFileSync(configPath, JSON.stringify(config, null), { flag: 'w' });
+        config.equalizer = {
+          reset: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          rock: [80, 103, 105, 121, 145, 128, 125, 123, 122, 143, 163, 134, 135, 129, 139, 146, 144, 153, 152, 149, 124, 102, 103],
+          electro: [99, 133, 102, 122, 100, 139, 125, 151, 158, 152, 124, 116, 116, 117, 147, 100, 139, 173, 112, 135, 165, 85, 121],
+          acustic: [104, 124, 141, 0, 0, 104, 0, 104, 117, 0, 0, 0, 107, 104, 109, 123, 92, 107, 0, 154, 113, 84, 90]
+        };
+        if (typeof config.musicFolder === 'string') {
+          config.musicFolder = [config.musicFolder];
+          fs.writeFileSync(configPath, JSON.stringify(config, null), { flag: 'w' });
+        }  
       }
     });
   }
