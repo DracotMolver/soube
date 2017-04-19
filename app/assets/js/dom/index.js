4e28d1eac24bb6976e1cf3285d1f38cdbad2e7e9
DracotMolver <diego.albertom@yahoo.cl>
2017-04-19 15:46:12 -0300

    improved the dom manipulation

--- app/assets/js/dom/index.js
+++ app/assets/js/dom/index.js
@@ -8,10 +8,15 @@ module.exports = (_ => {
   let createdElements = {};
   let poolOfElements = {};
   let event = {};
+  // let fragment = document.createDocumentFragment();
+  let el;
 
   /* --------------------------------- Events --------------------------------- */
   const event_ = {
     element: null,
+    switchClass: function (from, to) {
+      return el = this.element, el.className = el.className.replace(from, to), this;
+    },
     addClass: function (str) {
       const changeClassName = e => {
           let className = e.className.split(' ');
@@ -28,45 +33,44 @@ module.exports = (_ => {
       return this;
     },
     empty: function() {
-      let el = this.element;
+      el = this.element;
       while(el.firstChild) el.removeChild(el.firstChild);
 
       return this;
     },
-    text: function(str = null) {
-      if (str === null) return this.element.textContent;
-      if (this.element.length) this.element.forEach(e => e.innerHTML = `${str}`);
-      else this.element.innerHTML = `${str}`;
+    text: function (str = null) {
+      el = this.element;
+      if (str === null) return el.textContent;
+      if (el.length) el.forEach(e => e.innerHTML = str);
+      else el.innerHTML = str;
 
       return this;
     },
-    removeClass: function(_class) {
-      return this.element.className = this.element.className.replace(_class, '').trim(), this;
+    removeClass: function (_class) {
+      return el = this.element, el.className = el.className.replace(_class, '').trim(), this;
     },
     child: function(pos = -1) {
       return this.element = pos !== -1 ? this.element.children[pos] : Array.from(this.element.children),
       this;
     },
-    on: function(fn) {
+    on: function (fn) {
+      el = this.element;
       // Select element is like an array because of the options elements inside
-      if (this.element.length && this.element.nodeName !== 'SELECT')
-        this.element.forEach(e => onFunction(e, fn));
-      else onFunction(this.element, fn);
+      if (el.length && el.nodeName !== 'SELECT') el.forEach(e => onFunction(e, fn));
+      else onFunction(el, fn);
 
       return this;
     },
     rmEvent: function (...str) {
-      str.forEach(s => this.element[`on${s.toLowerCase()}`] = null)
-      return this;
+      return str.forEach(s => this.element[`on${s.toLowerCase()}`] = null), this;
     },
     data: function(data = null) {
       if (typeof data === 'string') {
         let d = this.element.dataset[data];
-        if (/^\d+$/.test(d)) return parseInt(d);
-        else if (/^\d+(\.+)\d+$/.test(d)) return parseFloat(d);
-        else if (/^(\w|\s)+$/.test(d)) return d.toString();
+        return /^\d+$/.test(d) ? parseInt(d) : (/^\d+\.+\d+$/.test(d) ? parseFloat(d) : d.toString());
       } else {
-        Object.keys(data).forEach(v => this.element.dataset[v] = data[v]);
+        el = this.element;
+        Object.keys(data).forEach(v => el.dataset[v] = data[v]);
       }
 
       return this;
@@ -75,23 +79,24 @@ module.exports = (_ => {
       return this.element.forEach((v, i) => fn.length === 1 ? fn(v) : fn(v, i)), this;
     },
     css: function (str) {
+      el = this.element;
       const cssChange = e => {
-          if (e.style.cssText.indexOf(str)) e.style.cssText += `${str};`;
-      }
+        if (e.style.cssText.indexOf(str)) e.style.cssText += `${str};`;
+      };
 
-      if (this.element.length) el.forEach(e => cssChange(e));
-      else cssChange(this.element);
+      if (el.length) el.forEach(e => cssChange(e));
+      else cssChange(el);
 
       return this;
     },
-    clone: function(isCloned) {
-      if (typeof this.element === 'string') {
-        return this.element = getCreatedElement(this.element).cloneNode(isCloned), this;
-      } else {
-        return event = Object.assign({}, event_),
-        event.element = this.element.cloneNode(isCloned), event;
-      }
-    },
+    // clone: function(isCloned) {
+    //   if (typeof this.element === 'string') {
+    //     return this.element = getCreatedElement(this.element).cloneNode(isCloned), this;
+    //   } else {
+    //     return event = Object.assign({}, event_),
+    //     event.element = this.element.cloneNode(isCloned), event;
+    //   }
+    // },
     get: function(pos = -1) {
       return pos === -1 ? this.element : this.element[pos];
     },
@@ -99,15 +104,14 @@ module.exports = (_ => {
       return this.element.removeAttribute(attr), this;
     },
     attr: function (attr) {
-      if (typeof attr === 'object')
-        Object.keys(attr).forEach(v => { this.element.setAttribute(v, attr[v]); });
-      else if (attr !== '')
-        return this.element.getAttribute(attr);
+      el = this.element;
+      if (typeof attr === 'object') Object.keys(attr).forEach(v => el.setAttribute(v, attr[v]));
+      else if (attr !== '') return el.getAttribute(attr);
 
       return this;
     },
-    insert: function (...a) {
-      return a.forEach(v => this.element.appendChild('element' in v ? v.element : v)), this;
+    append: function (...a) {
+      return el = this.element, a.forEach(v => el.appendChild('element' in v ? v.element : v)), this;
     },
     val: function (v = null) {
       return v === null ? this.element.value : this.element.value = v, this;
@@ -116,13 +120,13 @@ module.exports = (_ => {
       return this.element.className.indexOf(s) !== -1;
     },
     cssValue: function () {
-      const values = this.element.style.cssText;
-      return values.slice(0, values.length - 1).split(';').map(v => v.replace(/^[\w+\-]+\:\s+/g, ''));
+      return v = this.element.style.cssText,
+        v.slice(0, v.length - 1).split(';').map(v => v.replace(/^[\w+\-]+\:\s+/g, ''));
     }
   };
 
   /* --------------------------------- Functions --------------------------------- */
-   function onFunction (el, fn) {
+  function onFunction (el, fn) {
     Object.keys(fn).forEach(v => {
       /animation/.test(v) ?
         el.addEventListener(v.toLowerCase(), fn[v]) :
@@ -130,13 +134,14 @@ module.exports = (_ => {
     })
   }
 
-
-  function saveCreatedElement(name) {
-    if (!createdElements[name]) createdElements[name] = document.createElement(name);
-  }
+  // function saveCreatedElement(name) {
+  //   if (!createdElements[name]) createdElements[name] = document.createElement(name);
+  // }
 
   function getCreatedElement(name) {
-    return createdElements[name];
+    return !createdElements[name] ?
+      (createdElements[name] = document.createElement(name), createdElements[name]) :
+      createdElements[name].cloneNode(false);
   }
 
   function saveElementInPool(name, element) {
@@ -158,7 +163,7 @@ module.exports = (_ => {
     event = Object.assign({}, event_);
 
     if (inPool(e)) {
-      event.element = getElementInPool(e);
+      return event.element = getElementInPool(e), event;
     } else {
       if ((r = /^(\.|#|:)/.exec(e))) {
         switch(r[0]) {
@@ -168,13 +173,22 @@ module.exports = (_ => {
         }
 
         saveElementInPool(e);
-      } else if (typeof e === 'string') {
-        saveCreatedElement(e);
       }
+      // else if (typeof e === 'string') {
+      //   saveCreatedElement(e);
+      // }
     }
 
     return event.element = e, event;
   }
 
   _.$ = dom;
+
+  _.CreateElement = str => {
+    return event = Object.assign({
+      clone: function (deep = false) {
+        return this.element = getCreatedElement(this.element.nodeName.toLowerCase()).cloneNode(deep), this;
+      }
+    }, event_), event.element = getCreatedElement(str.toLowerCase()), event;
+  };
 })(global);
