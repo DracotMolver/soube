fac435fe58c4c03bb99897990dcce0f8f681e52b
DracotMolver <diego.albertom@yahoo.cl>
2017-04-23 22:31:07 -0300

    removed gulp - Didn't like me the css clean plugin

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const cleanCSS = require('gulp-clean-css');
-
-gulp.task('minify-css', () => {
-    return gulp.src('app/assets/css/main.css')
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('app/assets/css/dist'));
-});
