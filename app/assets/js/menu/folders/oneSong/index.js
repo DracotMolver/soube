a81f50eceb2ad4d089efb7f2994a69bdcb629edc
DracotMolver <diego.albertom@yahoo.cl>
2017-05-14 15:00:38 -0400

    read one song - WIP

---
+++ app/assets/js/menu/folders/oneSong/index.js
@@ -0,0 +1,28 @@
+/**
+ * @author Diego Alberto Molina Vera
+ * @copyright 2016 - 2017
+ */
+/* --------------------------------- Modules --------------------------------- */
+//---- Electron ----
+// const dialog = require('electron').remote.dialog;
+
+//---- Node ----
+// const path = require('path');
+
+//---- Own ----
+// const songFolder = require(path.join(__dirname, '../', 'songFolder'));
+// const {
+//   configFile,
+//   langFile
+// } = require(path.join(__dirname, '../../../', 'config')).init();
+// let player = require(path.join(__dirname, '../../../', 'player'));
+// const $ = require(path.join(__dirname, '../../../', 'dom'));
+
+/* --------------------------------- Variables --------------------------------- */
+const lang = langFile[configFile.lang];
+
+/* --------------------------------- Functions --------------------------------- */
+
+module.exports = Object.freeze({
+  loadSong
+});
